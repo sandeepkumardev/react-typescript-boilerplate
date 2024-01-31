848b0ad7368b05df5e31a36ad53e3245b3bc5e8d
sandeep <sandeepypb@gmail.com>
2024-01-31 21:30:19 +0530

    🔨 Refactor: add scripts for eslint & prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,6 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: [
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['prettier'],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
